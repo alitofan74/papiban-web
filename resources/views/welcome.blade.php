71533371d09b7a0f746326216aed9aaaad392ed5
shyntaayu <shynta.add@gmail.com>
2020-06-17 08:22:06 +0700

    + fixing welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,69 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Papi BAN</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{asset('bootstrap-4.5.0\dist\css\bootstrap.css')}}">
-        <style>
-        body{
+    <title>Papi BAN</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{asset('admin-templ/assets/css/app.min.css')}}">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{asset('admin-templ/assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('admin-templ/assets/css/components.css')}}">
+    <!-- Custom style CSS -->
+    <link rel="stylesheet" href="{{asset('admin-templ/assets/css/custom.css')}}">
+    <link rel='shortcut icon' type='image/x-icon' href="{{url('img\logo.ico')}}" />
+    <style>
+        body {
             font-family: "Nunito", "Segoe UI", arial;
         }
-        #head{
-            color:#557cf3;
+
+        #head {
+            color: #557cf3;
         }
-        </style>
-    </head>
-    <body>
-    
-    <div class="container" style="margin-top:2em;">
-        <div class="row">
-            <div class="col-12">
-                <h2 id="head">Papi Tambal Ban ( Pencarian Tambal Ban Terdekat )</h2>
+    </style>
+</head>
+
+<body>
+    <div class="loader"></div>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="page-error" style="padding:0;">
+                    <div class="page-inner">
+                        <h2 class="font-weight-bold" id="head">Papi Tambal Ban ( Pencarian Tambal Ban Terdekat )</h2>
+                        <div class="row">
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <img src="{{url('img\teropong.png')}}" width="90%" class="user-img mr-2" alt="">
+                            </div>
+                            <div class="col-sm-6 col-md-6 col-lg-6" style="margin-top: 6%;">
+                                <h4 class="font-weight-bold">Salam Warga Malang,</h4>
+                                <p class="my-1 page-description" style="line-height:normal;">Aplikasi untuk mencari tempat tambal ban, sesuai lokasi terdekat, atau pun sesuai dengan keinginan anda</p>
+
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom: 20%;">
+                            <div class="col-sm-6 col-md-6 col-lg-6"></div>
+                            <div class="col-sm-6 col-md-6 col-lg-6">
+                                <a style="border-radius:15px;width:100%; margin-right:0" class="btn btn-primary btn-block btn-lg" href="{{url('/login')}}">LOGIN</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-8">
-                <img src="{{url('img\teropong.png')}}" alt="" srcset="" class="img img-responsive" width="100%">
-            </div>
-            <div class="col-4" style="padding-top:10em;">
-                <h4>Salam Warga Malang,</h4>
-                <h6><p>Aplikasi untuk mencari tempat tambal ban, sesuai lokasi terdekat, atau pun sesuai dengan keinginan anda</p></h6>
-                <br>
-                <a style="width:60%;border-radius:15px;" class="btn btn-primary" href="{{url('/login')}}">LOGIN</a>
-            </div>
-        </div>
+        </section>
     </div>
 
 
-    <script src="{{asset('js\jquery-3.5.1.slim.min.js')}}"></script>
-    <script src="{{asset('bootstrap-4.5.0\dist\js\bootstrap.js')}}"></script>
-    </body>
+    <script src="{{asset('admin-templ/assets/js/app.min.js')}}"></script>
+    <!-- JS Libraies -->
+    <!-- Page Specific JS File -->
+    <!-- Template JS File -->
+    <script src="{{asset('admin-templ/assets/js/scripts.js')}}"></script>
+    <!-- Custom JS File -->
+    <script src="{{asset('admin-templ/assets/js/custom.js')}}"></script>
+</body>
+
 </html>
